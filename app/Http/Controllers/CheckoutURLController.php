5ee3fac54695ddc8a1a7e196206e401f01a8dd4d
Shams Pathan <suspathan@gmail.com>
2023-04-13 16:11:15 +0600

    BKASH_BASE_URL to .env

--- app/Http/Controllers/CheckoutURLController.php
+++ app/Http/Controllers/CheckoutURLController.php
@@ -14,27 +14,26 @@ class CheckoutURLController extends Controller
 
     public function __construct()
     {
-        // Sandbox
-        $this->base_url = 'https://tokenized.sandbox.bka.sh/v1.2.0-beta';
-        // Live
-        //$this->base_url = 'https://tokenized.pay.bka.sh/v1.2.0-beta';  
+        $this->base_url = env('BKASH_BASE_URL');
     }
 
-    public function authHeaders(){
+    public function authHeaders()
+    {
         return array(
             'Content-Type:application/json',
-            'Authorization:' .$this->grant(),
-            'X-APP-Key:'.env('BKASH_CHECKOUT_URL_APP_KEY')
+            'Authorization:' . $this->grant(),
+            'X-APP-Key:' . env('BKASH_CHECKOUT_URL_APP_KEY')
         );
     }
-         
-    public function curlWithBody($url,$header,$method,$body_data_json){
-        $curl = curl_init($this->base_url.$url);
-        curl_setopt($curl,CURLOPT_HTTPHEADER, $header);
-        curl_setopt($curl,CURLOPT_CUSTOMREQUEST, $method);
-        curl_setopt($curl,CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($curl,CURLOPT_POSTFIELDS, $body_data_json);
-        curl_setopt($curl,CURLOPT_FOLLOWLOCATION, 1);
+
+    public function curlWithBody($url, $header, $method, $body_data_json)
+    {
+        $curl = curl_init($this->base_url . $url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $body_data_json);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
         curl_setopt($curl, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
         $response = curl_exec($curl);
         curl_close($curl);
@@ -44,16 +43,16 @@ class CheckoutURLController extends Controller
     public function grant()
     {
         $header = array(
-                'Content-Type:application/json',
-                'username:'.env('BKASH_CHECKOUT_URL_USER_NAME'),
-                'password:'.env('BKASH_CHECKOUT_URL_PASSWORD')
-                );
-        $header_data_json=json_encode($header);
+            'Content-Type:application/json',
+            'username:' . env('BKASH_CHECKOUT_URL_USER_NAME'),
+            'password:' . env('BKASH_CHECKOUT_URL_PASSWORD')
+        );
+        $header_data_json = json_encode($header);
 
-        $body_data = array('app_key'=> env('BKASH_CHECKOUT_URL_APP_KEY'), 'app_secret'=>env('BKASH_CHECKOUT_URL_APP_SECRET'));
-        $body_data_json=json_encode($body_data);
-    
-        $response = $this->curlWithBody('/tokenized/checkout/token/grant',$header,'POST',$body_data_json);
+        $body_data = array('app_key' => env('BKASH_CHECKOUT_URL_APP_KEY'), 'app_secret' => env('BKASH_CHECKOUT_URL_APP_SECRET'));
+        $body_data_json = json_encode($body_data);
+
+        $response = $this->curlWithBody('/tokenized/checkout/token/grant', $header, 'POST', $body_data_json);
 
         $token = json_decode($response)->id_token;
 
@@ -67,22 +66,22 @@ class CheckoutURLController extends Controller
 
     public function createPayment(Request $request)
     {
-        $header =$this->authHeaders();
+        $header = $this->authHeaders();
 
         $website_url = URL::to("/");
 
         $body_data = array(
             'mode' => '0011',
             'payerReference' => ' ',
-            'callbackURL' => $website_url.'/bkash/callback',
+            'callbackURL' => $website_url . '/bkash/callback',
             'amount' => $request->amount ? $request->amount : 10,
             'currency' => 'BDT',
             'intent' => 'sale',
-            'merchantInvoiceNumber' => "Inv".Str::random(8) // you can pass here OrderID 
+            'merchantInvoiceNumber' => "Inv" . Str::random(8) // you can pass here OrderID 
         );
-        $body_data_json=json_encode($body_data);
+        $body_data_json = json_encode($body_data);
 
-        $response = $this->curlWithBody('/tokenized/checkout/create',$header,'POST',$body_data_json);
+        $response = $this->curlWithBody('/tokenized/checkout/create', $header, 'POST', $body_data_json);
 
         return redirect((json_decode($response)->bkashURL));
     }
@@ -90,21 +89,21 @@ class CheckoutURLController extends Controller
     public function executePayment($paymentID)
     {
 
-        $header =$this->authHeaders();
+        $header = $this->authHeaders();
 
         $body_data = array(
             'paymentID' => $paymentID
         );
-        $body_data_json=json_encode($body_data);
+        $body_data_json = json_encode($body_data);
 
-        $response = $this->curlWithBody('/tokenized/checkout/execute',$header,'POST',$body_data_json);
+        $response = $this->curlWithBody('/tokenized/checkout/execute', $header, 'POST', $body_data_json);
 
-        $res_array = json_decode($response,true);
+        $res_array = json_decode($response, true);
 
-        if(isset($res_array['trxID'])){
+        if (isset($res_array['trxID'])) {
             // your database insert operation
             // save $response
-        
+
         }
 
         return $response;
@@ -113,50 +112,48 @@ class CheckoutURLController extends Controller
     public function queryPayment($paymentID)
     {
 
-        $header =$this->authHeaders();
+        $header = $this->authHeaders();
 
         $body_data = array(
             'paymentID' => $paymentID,
         );
-        $body_data_json=json_encode($body_data);
+        $body_data_json = json_encode($body_data);
 
-        $response = $this->curlWithBody('/tokenized/checkout/payment/status',$header,'POST',$body_data_json);
-        
-        $res_array = json_decode($response,true);
-        
-        if(isset($res_array['trxID'])){
+        $response = $this->curlWithBody('/tokenized/checkout/payment/status', $header, 'POST', $body_data_json);
+
+        $res_array = json_decode($response, true);
+
+        if (isset($res_array['trxID'])) {
             // your database insert operation
             // insert $response to your db
-        
+
         }
 
-         return $response;
+        return $response;
     }
 
     public function callback(Request $request)
     {
         $allRequest = $request->all();
-        if(isset($allRequest['status']) && $allRequest['status'] == 'failure'){
+        if (isset($allRequest['status']) && $allRequest['status'] == 'failure') {
             return view('CheckoutURL.fail')->with([
                 'response' => 'Payment Failure'
             ]);
-
-        }else if(isset($allRequest['status']) && $allRequest['status'] == 'cancel'){
+        } else if (isset($allRequest['status']) && $allRequest['status'] == 'cancel') {
             return view('CheckoutURL.fail')->with([
                 'response' => 'Payment Cancell'
             ]);
+        } else {
 
-        }else{
-            
             $response = $this->executePayment($allRequest['paymentID']);
 
-            $arr = json_decode($response,true);
-    
-            if(array_key_exists("statusCode",$arr) && $arr['statusCode'] != '0000'){
+            $arr = json_decode($response, true);
+
+            if (array_key_exists("statusCode", $arr) && $arr['statusCode'] != '0000') {
                 return view('CheckoutURL.fail')->with([
                     'response' => $arr['statusMessage'],
                 ]);
-            }else if(array_key_exists("message",$arr)){
+            } else if (array_key_exists("message", $arr)) {
                 // if execute api failed to response
                 sleep(1);
                 $query = $this->queryPayment($allRequest['paymentID']);
@@ -164,13 +161,11 @@ class CheckoutURLController extends Controller
                     'response' => $query
                 ]);
             }
-    
+
             return view('CheckoutURL.success')->with([
                 'response' => $response
             ]);
-
         }
-
     }
 
     public function getRefund(Request $request)
@@ -180,7 +175,7 @@ class CheckoutURLController extends Controller
 
     public function refundPayment(Request $request)
     {
-        $header =$this->authHeaders();
+        $header = $this->authHeaders();
 
         $body_data = array(
             'paymentID' => $request->paymentID,
@@ -189,17 +184,16 @@ class CheckoutURLController extends Controller
             'sku' => 'sku',
             'reason' => 'Quality issue'
         );
-     
-        $body_data_json=json_encode($body_data);
 
-        $response = $this->curlWithBody('/tokenized/checkout/payment/refund',$header,'POST',$body_data_json);
-        
+        $body_data_json = json_encode($body_data);
+
+        $response = $this->curlWithBody('/tokenized/checkout/payment/refund', $header, 'POST', $body_data_json);
+
         // your database operation
         // save $response
-        
+
         return view('CheckoutURL.refund')->with([
             'response' => $response,
         ]);
-    }        
-    
+    }
 }
